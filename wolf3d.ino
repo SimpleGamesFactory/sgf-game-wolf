adde296d8e424d5c3c088eed1a5e38f0698479b9
Marcin Nowak <marcin.j.nowak@gmail.com>
2026-03-12 23:13:31 +0100

    spi freq

--- wolf3d.ino
+++ wolf3d.ino
@@ -7,6 +7,10 @@
 // Example:
 // #define SGF_HW_PRESET SGF_HW_PRESET_ESP32_ST7789_240X240
 
+#ifndef SGF_ESP32_ST7789_240X240_SPI_HZ
+#define SGF_ESP32_ST7789_240X240_SPI_HZ 40000000u
+#endif
+
 #include "SGFHardwarePresets.h"
 #include "Game.h"
 
